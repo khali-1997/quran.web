9279e2b3134b8159dfd14ea3654ac7b0bde2660e
Reza <rm.biqarar@gmail.com>
2019-04-09 17:40:26 +0430

    fix filter and sort audio bank

--- content/audio/view.php
+++ content/audio/view.php
@@ -17,10 +17,13 @@ class view
 			\dash\data::page_title(\dash\data::page_title(). ' | '. T_('Search for :search', ['search' => $search_string]));
 		}
 
+		$filterArgs = [];
+
 		$args =
 		[
-			'sort'  => \dash\request::get('sort'),
-			'order' => \dash\request::get('order'),
+			'sort'       => \dash\request::get('sort'),
+			'order'      => \dash\request::get('order'),
+			'pagenation' => false,
 		];
 
 		if(!$args['order'])
@@ -31,42 +34,49 @@ class view
 
 		if(!$args['sort'])
 		{
-			$args['sort'] = 'sort';
+			$args['sort'] = 'id';
 		}
 
 		if(\dash\request::get('status'))
 		{
-			$args['status'] = \dash\request::get('status');
+			$args['status']       = \dash\request::get('status');
+			$filterArgs['status'] = \dash\request::get('status');
 		}
 
 		if(\dash\request::get('type'))
 		{
-			$args['type'] = \dash\request::get('type');
+			$args['type']       = \dash\request::get('type');
+			$filterArgs['type'] = \dash\request::get('type');
 		}
 
 		if(\dash\request::get('qari'))
 		{
-			$args['qari'] = \dash\request::get('qari');
+			$args['qari']       = \dash\request::get('qari');
+			$filterArgs['qari'] = \dash\request::get('qari');
 		}
 
 		if(\dash\request::get('readtype'))
 		{
-			$args['readtype'] = \dash\request::get('readtype');
+			$args['readtype']       = \dash\request::get('readtype');
+			$filterArgs['readtype'] = \dash\request::get('readtype');
 		}
 
 		if(\dash\request::get('filetype'))
 		{
-			$args['filetype'] = \dash\request::get('filetype');
+			$args['filetype']       = \dash\request::get('filetype');
+			$filterArgs['filetype'] = \dash\request::get('filetype');
 		}
 
 		if(\dash\request::get('country'))
 		{
-			$args['country'] = \dash\request::get('country');
+			$args['country']       = \dash\request::get('country');
+			$filterArgs['country'] = \dash\request::get('country');
 		}
 
 		if(\dash\request::get('quality'))
 		{
-			$args['quality'] = \dash\request::get('quality');
+			$args['quality']       = \dash\request::get('quality');
+			$filterArgs['quality'] = \dash\request::get('quality');
 		}
 
 
@@ -76,12 +86,10 @@ class view
 		\dash\data::sortLink($sortLink);
 		\dash\data::dataTable($dataTable);
 
-		$check_empty_datatable = $args;
-		unset($check_empty_datatable['sort']);
-		unset($check_empty_datatable['order']);
+
 
 		// set dataFilter
-		$dataFilter = \dash\app\sort::createFilterMsg($search_string, $check_empty_datatable);
+		$dataFilter = \dash\app\sort::createFilterMsg($search_string, $filterArgs);
 		\dash\data::dataFilter($dataFilter);
 
 
