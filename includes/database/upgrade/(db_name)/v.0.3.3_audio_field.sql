cb824ac97eafc772472424d4526918ef710d0471
Reza <rm.biqarar@gmail.com>
2019-04-10 19:01:35 +0430

    fix field name

--- includes/database/upgrade/(db_name)/v.0.3.3_audio_field.sql
+++ includes/database/upgrade/(db_name)/v.0.3.3_audio_field.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `audiobank` ADD `translator`  varchar(200)  NULL DEFAULT NULL ;
-ALTER TABLE `audiobank` ADD `language`  varchar(50)  NULL DEFAULT NULL ;
 ALTER TABLE `audiobank` ADD `reader`  varchar(200)  NULL DEFAULT NULL ;
+ALTER TABLE `audiobank` ADD `language`  varchar(50)  NULL DEFAULT NULL ;
+ALTER TABLE `audiobank` ADD `qari2`  varchar(200)  NULL DEFAULT NULL ;
