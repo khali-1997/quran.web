beabb309ca0926e543fbfebf85b4ac713a01e87f
Reza <rm.biqarar@gmail.com>
2019-04-18 16:27:20 +0430

    add juz hizb detail

--- content_api/v6/detail/controller.php
+++ content_api/v6/detail/controller.php
@@ -19,6 +19,10 @@ class controller
 				$data = self::juz_sura();
 				break;
 
+			case 'juz-hizb':
+				$data = self::juz_hizb();
+				break;
+
 			default:
 				\content_api\v6::no(404);
 				break;
@@ -36,6 +40,153 @@ class controller
 			$get = \dash\file::read($dir);
 			$get = json_decode($get, true);
 			return $get;
+		}
+
+		return null;
+	}
+
+
+	private static function juz_hizb()
+	{
+		$dir = __DIR__. '/juz-hizb.json';
+		if(is_file($dir))
+		{
+			$get = \dash\file::read($dir);
+			$get = json_decode($get, true);
+			return $get;
+		}
+
+		return null;
+	}
+
+
+	private static function make_juz_hizb()
+	{
+		$data       = \lib\db\quran::get(['1.1' => 1.1]);
+		$result     = [];
+		$index_rub  = 0;
+		$index_hizb = 0;
+		foreach ($data as $key => $value)
+		{
+			if(!isset($result[$value['juz']]))
+			{
+				$index_hizb = 0;
+				$result[$value['juz']] = [];
+			}
+
+			if(!isset($result[$value['juz']][$value['hizb']]))
+			{
+				$index_rub = 0;
+				$index_hizb++;
+				$result[$value['juz']][$value['hizb']] = [];
+			}
+
+			if(!isset($result[$value['juz']][$value['hizb']][$value['rub']]))
+			{
+				$index_rub++;
+				$text = $value['text'];
+				$text = explode(' ', $text);
+				$x = 0;
+				$my_Text = [];
+				foreach ($text as $v)
+				{
+					$x++;
+					if($x === 7)
+					{
+						break;
+					}
+					$my_Text[] = $v;
+					# code...
+				}
+				$my_Text = implode(' ', $my_Text);
+
+				$text = $value['simple'];
+				$text = explode(' ', $text);
+				$x = 0;
+				$my_Text_simple = [];
+				foreach ($text as $v)
+				{
+					$x++;
+					if($x === 7)
+					{
+						break;
+					}
+					$my_Text_simple[] = $v;
+					# code...
+				}
+				$my_Text_simple = implode(' ', $my_Text_simple);
+
+				$result[$value['juz']][$value['hizb']][$value['rub']] =
+				[
+					'index_rub'  => $index_rub,
+					'index_hizb' => $index_hizb,
+					'rub'        => $value['rub'],
+					'sura'       => $value['sura'],
+					'page'       => $value['page'],
+					'sura_detail' => \lib\app\sura::detail($value['sura']),
+					'aya'        => $value['aya'],
+					'text'       => $value['text'],
+					'simple'     => $value['simple'],
+					'first_word' => $my_Text,
+					'first_word_simple' => $my_Text_simple,
+
+				];
+			}
+		}
+
+		j($result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+		if(is_file($dir))
+		{
+			$get = \dash\file::read($dir);
+			$get = json_decode($get, true);
+
+			if(!is_array($get))
+			{
+				return null;
+			}
+
+			$result = [];
+			$last_juz_sura = [];
+
+			foreach ($get as $key => $value)
+			{
+				$temp              = [];
+				$temp['index']     = $value['index'];
+				$temp['startpage'] = $value['startpage'];
+				$temp['sura']      = [];
+
+				foreach ($value['allsura'] as $k => $v)
+				{
+					if(in_array($v, $last_juz_sura))
+					{
+						continue;
+					}
+					$temp['sura'][] = \lib\app\sura::detail($v);
+				}
+
+				$last_juz_sura = $value['allsura'];
+
+				if(empty($temp['sura']))
+				{
+					unset($temp['sura']);
+				}
+				$result[] = $temp;
+			}
+
+			return $result;
 		}
 
 		return null;
