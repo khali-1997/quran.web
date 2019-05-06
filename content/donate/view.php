4b2f0ebffe42f6a05507d1edf551313923e8dbc0
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-06 11:43:05 +0430

    add donate page

--- content/donate/view.php
+++ content/donate/view.php
@@ -7,7 +7,9 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Donate to SalamQuran'));
-		\dash\data::page_desc(T_('Soon'));
+		\dash\data::page_desc(T_('Help this project grow faster and better.'));
+		\dash\data::page_special(true);
+
 	}
 }
 ?>
