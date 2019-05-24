039da18d80faca23a8134cea7d0423a00b0fa4bb
Javad Kakroudi <J.Evazzadeh@gmail.com>
2019-05-24 09:47:48 +0430

    update donate page

--- content/donate/view.php
+++ content/donate/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		// \dash\data::page_title(T_('Donate to SalamQuran'));
-		\dash\data::page_title('خرید اپ اندروید');
+		\dash\data::page_title('خرید  فایل لوگو');
 		\dash\data::page_desc(T_('Help this project grow faster and better.'));
 		\dash\data::page_special(true);
 
