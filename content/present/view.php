22a7da676e07741dead260fe28708ea4babdb5db
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-31 16:44:00 +0430

    align center present page with flex

--- content/present/view.php
+++ content/present/view.php
@@ -8,6 +8,10 @@ class view
 	{
 		\dash\data::page_title(T_('Present Quran'));
 		\dash\data::page_desc(T_('Present like TV'));
+
+		\dash\data::include_js(false);
+		\dash\data::bodyclass('present');
+
 	}
 }
 ?>
