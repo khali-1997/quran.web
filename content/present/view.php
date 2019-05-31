cd668707c5c29a7ee3d44c353f0924d5be94ed28
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-31 15:15:15 +0430

    add present page

---
+++ content/present/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\present;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('Present Quran'));
+		\dash\data::page_desc(T_('Present like TV'));
+	}
+}
+?>
