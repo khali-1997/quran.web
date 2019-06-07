7036f3ed464bd255ad36e0c999162edde6faed75
Reza <rm.biqarar@gmail.com>
2019-06-07 06:38:37 +0430

    add custom sitempa

---
+++ includes/lib/sitemap.php
@@ -0,0 +1,14 @@
+<?php
+namespace lib;
+
+
+class sitemap
+{
+	public static function create(&$sitemap)
+	{
+		// $sitemap->addItem($myUrl, '0.5', 'weekly', $row['publishdate']);
+		\dash\utility\sitemap::show_result('sura', 114);
+
+	}
+}
+?>
