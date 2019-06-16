e7c42aec5d4fec60a2fa1b298fec21944e448b18
Reza <rm.biqarar@gmail.com>
2019-06-16 12:17:16 +0430

    fix get juz detail

--- includes/lib/app/juz.php
+++ includes/lib/app/juz.php
@@ -4,33 +4,43 @@ namespace lib\app;
 
 class juz
 {
-
-	public static function detail($_id, $_field = null)
+	public static function list()
 	{
 		$addr = root. '/content_api/v6/juz/juz.json';
+		$get = [];
 		if(is_file($addr))
 		{
 			$get = \dash\file::read($addr);
 			$get = json_decode($get, true);
-			if(is_array($get))
+			if(!is_array($get))
 			{
-				if(isset($get[$_id]))
-				{
-					if(!$_field)
-					{
-						return $get[$_id];
-					}
-					elseif(isset($get[$_id][$_field]))
-					{
-						return $get[$_id][$_field];
-					}
-					else
-					{
-						return null;
-					}
-				}
+				$get = [];
 			}
 		}
+		return $get;
+	}
+
+
+	public static function detail($_id, $_field = null)
+	{
+		$get = self::list();
+
+		if(isset($get[$_id]))
+		{
+			if(!$_field)
+			{
+				return $get[$_id];
+			}
+			elseif(isset($get[$_id][$_field]))
+			{
+				return $get[$_id][$_field];
+			}
+			else
+			{
+				return null;
+			}
+		}
+
 		return null;
 	}
 }
