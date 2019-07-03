b1ec0ce48c5932b94d2655871af31d56b950898b
Reza <rm.biqarar@gmail.com>
2019-07-03 17:31:06 +0430

    add quick access

--- includes/lib/app/quick_access.php
+++ includes/lib/app/quick_access.php
@@ -8,11 +8,40 @@ class quick_access
 	public static function list()
 	{
 		$list = [];
+
 		$list[] =
 		[
-			'title' => 'Fatiha',
-			'desc'  => 'fatiha alketab',
+			'title' => T_('Al-Fatihah'),
+			'desc'  => null,
 			'url'   => \dash\url::kingdom(). '/s1',
+		];
+
+		$list[] =
+		[
+			'title' => T_("Ar-Rahman"),
+			'desc'  => null,
+			'url'   => \dash\url::kingdom(). '/s55',
+		];
+
+		$list[] =
+		[
+			'title' => T_('Al-Mulk'),
+			'desc'  => null,
+			'url'   => \dash\url::kingdom(). '/s67',
+		];
+
+		$list[] =
+		[
+			'title' => T_('Ya-Sin'),
+			'desc'  => null,
+			'url'   => \dash\url::kingdom(). '/s36',
+		];
+
+		$list[] =
+		[
+			'title' => T_("Al-Waqi'ah"),
+			'desc'  => null,
+			'url'   => \dash\url::kingdom(). '/s56',
 		];
 		return $list;
 	}
