3c5cffe4c5a7f22f0aa36e5193e576b2f92cac18
Reza <rm.biqarar@gmail.com>
2019-07-14 13:22:29 +0430

    check language post in mag

--- content_m/mag/connect/view.php
+++ content_m/mag/connect/view.php
@@ -5,8 +5,9 @@ class view
 {
 	public static function config()
 	{
-		$args               = [];
-		$args['type']       = 'post';
+		$args             = [];
+		$args['type']     = 'post';
+		$args['language'] = \dash\language::current();
 		$post_list = \dash\app\posts::all_post_title($args);
 
 		\dash\data::postList($post_list);
