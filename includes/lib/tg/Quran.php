50ce2d56d5707957793fb0d9bfcb2bd51d1ad382
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-07-17 13:38:36 +0430

    fix juz and surah nav

--- includes/lib/tg/Quran.php
+++ includes/lib/tg/Quran.php
@@ -88,7 +88,7 @@ class Quran
 	public static function requireCode()
 	{
 		bot::ok();
-		$msg = T_("We need a number!")." 🙁";
+		$msg = T_("We need a valid number!")." 🙁";
 
 		// if start with callback answer callback
 		if(bot::isCallback())
@@ -117,7 +117,7 @@ class Quran
 		{
 			if(is_numeric($_pageNumber))
 			{
-				if($_pageNumber < 1 && $_pageNumber > 604)
+				if($_pageNumber < 1 || $_pageNumber > 604)
 				{
 					return self::requireCode();
 				}
@@ -225,20 +225,21 @@ class Quran
 
 
 
-	public static function juz($_code)
+	public static function juz($_juz)
 	{
-		if($_code)
+		if($_juz)
 		{
-			if(is_numeric($_code))
+			if(is_numeric($_juz))
 			{
-				if($_code < 1 && $_code > 30)
+				if($_juz < 1 || $_juz > 30)
 				{
 					return self::requireCode();
 				}
 			}
 			else
 			{
-				return self::page(mt_rand(1, 30), 'juz', true);
+				$startPage = self::page_of_juz(mt_rand(1, 30));
+				return self::page($startPage, 'juz', true);
 			}
 
 		}
@@ -261,16 +262,223 @@ class Quran
 			return true;
 		}
 
+		$startPage = self::page_of_juz($_juz);
 
-		return self::page($_code, 'juz');
+		return self::page($startPage, 'juz');
 	}
 
 
-	public static function surah($_code)
+	public static function page_of_juz($_juz)
 	{
-		return  self::page($_code, 'surah');
+		$juz_startpages =
+		[
+			1  => 1,
+			2  => 22,
+			3  => 42,
+			4  => 62,
+			5  => 82,
+			6  => 102,
+			7  => 121,
+			8  => 142,
+			9  => 162,
+			10 => 182,
+			11 => 201,
+			12 => 222,
+			13 => 242,
+			14 => 262,
+			15 => 282,
+			16 => 302,
+			17 => 322,
+			18 => 342,
+			19 => 362,
+			20 => 382,
+			21 => 402,
+			22 => 422,
+			23 => 442,
+			24 => 462,
+			25 => 482,
+			26 => 502,
+			27 => 522,
+			28 => 542,
+			29 => 562,
+			30 => 582
+		];
+
+		if(isset($juz_startpages[$_juz]))
+		{
+			return $juz_startpages[$_juz];
+		}
 	}
 
+
+	public static function surah($_surah)
+	{
+		if($_surah)
+		{
+			if(is_numeric($_surah))
+			{
+				if($_surah < 1 || $_surah > 114)
+				{
+					return self::requireCode();
+				}
+			}
+			else
+			{
+				$startPage = self::page_of_surah(mt_rand(1, 114));
+				return self::page($startPage, 'surah', true);
+			}
+
+		}
+		else
+		{
+			bot::ok();
+			$randomVal = mt_rand(1, 114);
+
+			// we dont have number show help of juz
+			$msg = '';
+			$msg .= "<b>". T_('SalamQuran'). "</b>". "\n";
+			$msg .= T_('For access to specefic surah of Quran please use and type one of below syntax')."\n";
+			$msg .= "<code>س". $randomVal. "</code>"."\n";
+			$msg .= "<code>s". $randomVal. "</code>"."\n";
+			$msg .= "/s". $randomVal. "\n";
+
+			$msg .= "\n";
+			$msg .= bot::website();
+			bot::sendMessage($msg);
+			return true;
+		}
+
+		$startPage = self::page_of_surah($_surah);
+
+		return  self::page($startPage, 'surah');
+	}
+
+
+	public static function page_of_surah($_surah)
+	{
+		$sura_startpages =
+		[
+			1   => 1,
+			2   => 2,
+			3   => 50,
+			4   => 77,
+			5   => 106,
+			6   => 128,
+			7   => 151,
+			8   => 177,
+			9   => 187,
+			10  => 208,
+			11  => 221,
+			12  => 235,
+			13  => 249,
+			14  => 255,
+			15  => 262,
+			16  => 267,
+			17  => 282,
+			18  => 293,
+			19  => 305,
+			20  => 312,
+			21  => 322,
+			22  => 332,
+			23  => 342,
+			24  => 350,
+			25  => 359,
+			26  => 367,
+			27  => 377,
+			28  => 385,
+			29  => 396,
+			30  => 404,
+			31  => 411,
+			32  => 415,
+			33  => 418,
+			34  => 428,
+			35  => 434,
+			36  => 440,
+			37  => 446,
+			38  => 453,
+			39  => 458,
+			40  => 467,
+			41  => 477,
+			42  => 483,
+			43  => 489,
+			44  => 496,
+			45  => 499,
+			46  => 502,
+			47  => 507,
+			48  => 511,
+			49  => 515,
+			50  => 518,
+			51  => 520,
+			52  => 523,
+			53  => 526,
+			54  => 528,
+			55  => 531,
+			56  => 534,
+			57  => 537,
+			58  => 542,
+			59  => 545,
+			60  => 549,
+			61  => 551,
+			62  => 553,
+			63  => 554,
+			64  => 556,
+			65  => 558,
+			66  => 560,
+			67  => 562,
+			68  => 564,
+			69  => 566,
+			70  => 568,
+			71  => 570,
+			72  => 572,
+			73  => 574,
+			74  => 575,
+			75  => 577,
+			76  => 578,
+			77  => 580,
+			78  => 582,
+			79  => 583,
+			80  => 585,
+			81  => 586,
+			82  => 587,
+			83  => 587,
+			84  => 589,
+			85  => 590,
+			86  => 591,
+			87  => 591,
+			88  => 592,
+			89  => 593,
+			90  => 594,
+			91  => 595,
+			92  => 595,
+			93  => 596,
+			94  => 596,
+			95  => 597,
+			96  => 597,
+			97  => 598,
+			98  => 598,
+			99  => 599,
+			100 => 599,
+			101 => 600,
+			102 => 600,
+			103 => 601,
+			104 => 601,
+			105 => 601,
+			106 => 602,
+			107 => 602,
+			108 => 602,
+			109 => 603,
+			110 => 603,
+			111 => 603,
+			112 => 604,
+			113 => 604,
+			114 => 604
+		];
+
+		if(isset($sura_startpages[$_surah]))
+		{
+			return $sura_startpages[$_surah];
+		}
+	}
 
 
 	public static function aya($_code)
