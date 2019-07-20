5294986d349cac5af3d6344ed1975b1a2d509c3b
Reza <rm.biqarar@gmail.com>
2019-07-20 13:47:46 +0430

    add subtype to mags

--- includes/database/upgrade/(db_name)/v.3.6_mag_word.sql
+++ includes/database/upgrade/(db_name)/v.3.6_mag_word.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `mags` ADD `word`  int(10) unsigned  NULL DEFAULT NULL ;
 ALTER TABLE `mags` ADD `wordtitle`  varchar(100)   NULL DEFAULT NULL ;
+ALTER TABLE `mags` ADD `subtype`  varchar(500)   NULL DEFAULT NULL ;
