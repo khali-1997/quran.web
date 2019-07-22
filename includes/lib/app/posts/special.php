d6ed3fd5e79040b38a4f89b4bebfa51ac3dce92e
Reza <rm.biqarar@gmail.com>
2019-07-22 19:17:28 +0430

    fill mag by real value

--- includes/lib/app/posts/special.php
+++ includes/lib/app/posts/special.php
@@ -8,6 +8,7 @@ class special
 	public static function list()
 	{
 		$list             = [];
+
 		$list['mustread'] = T_("Must read");
 		$list['editor']   = T_("Editor selection");
 		$list['special']  = T_("Special");
