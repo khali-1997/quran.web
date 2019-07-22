c72ca61709a886f18d4704db316048965d9c2893
Reza <rm.biqarar@gmail.com>
2019-07-22 18:44:47 +0430

    show category list

--- content/mag/view.php
+++ content/mag/view.php
@@ -6,6 +6,10 @@ class view
 	public static function config()
 	{
 		\dash\data::display_magAdmin('content/mag/dashboard.html');
+
+
+		$mag_count = \dash\app\posts::category_count();
+		\dash\data::categoryCount($mag_count);
 	}
 }
 ?>
