2f78d6b5d1a5aa1b322e3b95e1718945d316284f
Reza <rm.biqarar@gmail.com>
2019-08-04 11:39:26 +0430

    check true value in quran from to navigation

--- content/fromto/view.php
+++ content/fromto/view.php
@@ -14,10 +14,26 @@ class view
 		$startaya = \dash\request::get('startaya');
 		$endaya   = \dash\request::get('endaya');
 
-		if($sura && $startaya && $endaya)
+		if(is_numeric($sura) && is_numeric($startaya) && is_numeric($endaya))
 		{
-			\dash\data::fromtoLink('f'. $sura. '-'. $startaya. '-'. $endaya);
+
+			if($sura < 1 || $sura > 114 || $startaya < 1 || $startaya > $endaya  || $endaya > intval(\lib\app\sura::detail($sura, 'ayas')))
+			{
+				\dash\notif::error(T_("Invalid value"), ['element' => ['startaya', 'endaya']]);
+				return false;
+			}
+			else
+			{
+				\dash\data::fromtoLink('f'. $sura. '-'. $startaya. '-'. $endaya);
+			}
+
 		}
+		else
+		{
+			\dash\notif::error(T_("Please set the value as a number"), ['element' => ['startaya', 'endaya']]);
+		}
+
 	}
 }
+
 ?>
