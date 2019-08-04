026fae06e394da68f7ed67830aa920938dcfbabb
Reza <rm.biqarar@gmail.com>
2019-08-04 16:23:08 +0430

    save opt file in exam

--- content_m/level/exam/model.php
+++ content_m/level/exam/model.php
@@ -16,9 +16,13 @@ class model
 		$post                = [];
 		$post['title']       = \dash\request::post('title');
 		$post['opt1']        = \dash\request::post('opt1');
+		$post['opt1file']    = \dash\app\file::upload_quick('opt1file');
 		$post['opt2']        = \dash\request::post('opt2');
+		$post['opt2file']    = \dash\app\file::upload_quick('opt2file');
 		$post['opt3']        = \dash\request::post('opt3');
+		$post['opt3file']    = \dash\app\file::upload_quick('opt3file');
 		$post['opt4']        = \dash\request::post('opt4');
+		$post['opt4file']    = \dash\app\file::upload_quick('opt4file');
 		$post['lm_level_id'] = \dash\request::get('id');
 		$post['trueopt']     = \dash\request::post('trueopt');
 
